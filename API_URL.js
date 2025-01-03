1dfef765d16180872badab93041ae9221d2b877d
kamalMakarim <kamal.makarim@gmail.com>
2025-01-03 09:55:20 +0700

    change API_URL

--- API_URL.js
+++ API_URL.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://bukom-be-prod.9retes.easypanel.host/";
 // export const API_URL = "https://bukom-binekas-be.vercel.app";
