ffc6d8acec66a49d7ba8f179800f2c2221430859
kamalMakarim <kamal.makarim@gmail.com>
2025-01-10 10:13:40 +0700

    vite config for proxy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   if (process.env.NODE_ENV === "development") {
     return {
       plugins: [react()],
-    }
+    };
   } else {
     return {
       base: "/",
@@ -20,7 +20,15 @@ export default defineConfig(({ command }) => {
         strictPort: true,
         host: true,
         origin: "http://0.0.0.0:8081",
+        proxy: {
+          "/api": {
+            target: "https://be-bukom.sekolahbinekas.com", // Backend URL
+            changeOrigin: true, // This ensures the origin is passed correctly
+            secure: true, // If your backend uses HTTPS
+            rewrite: (path) => path.replace(/^\/api/, ""), // Optional path rewrite
+          },
+        },
       },
-    }
+    };
   }
-})
+});
