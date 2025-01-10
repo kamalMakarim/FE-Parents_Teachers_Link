294bfcf262689483a4c5e1d535294dac745eae84
kamalMakarim <kamal.makarim@gmail.com>
2025-01-10 09:00:12 +0700

    changes to api url

--- API_URL.js
+++ API_URL.js
@@ -1,2 +1,3 @@
 //export const API_URL = "http://localhost:3000";
-export const API_URL = "https://binekas-bukom-be-prod.tagj8z.easypanel.host";
+//export const API_URL = "https://binekas-bukom-be-prod.tagj8z.easypanel.host";
+export const API_URL = "https://be-bukom.sekolahbinekas.com";
